c24d48f1ba51feb157daff39ad356f614c28db1b
mderosa777 <meggsnbacon777@gmail.com>
2020-10-22 21:22:47 -0500

    styling

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -17,10 +17,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const postSpan = document.createElement("span");
     postSpan.innerText = post.content;
     postSpan.className = "postSpan";
-
+    postSpan.style.color = red;
     const img = document.createElement("img");
     img.src = post.img_url;
     img.width = '300'
+    img.style.backgroundColor = 'transparent'; 
 
 
     postDiv.append(postSpan, img);
@@ -56,5 +57,15 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch(postsURL, newPost).then(addPost(post));
   }
 
+  //function backGround(pic){
+
+    //const b = document.querySelector("body")
+    //const img = document.createElement("bg-image")
+    //img.className = ('bg')
+    //img.src = "https://images.unsplash.com/photo-1512675559587-8a91082206ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1255&q=80"
+  }
+
+   b.append(img)
+   backGround(pic);
   
 });
